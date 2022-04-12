6a2b8b1043c8e35250b9388c75a04ad0ea7aa5d6
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2022-04-11 20:10:10 -0500

    add/ axios + promises fetchid data form API

--- src/promises.ts
+++ src/promises.ts
@@ -10,9 +10,26 @@ import axios from "axios";
       });
       return promise;
     }
+
     console.log('Called and waiting...');
     const rta = await delay(3000);
-    console.log(rta);
+    console.log({rta});
+
+    function getProducts(){
+      const promise = axios.get('https://api.escuelajs.co/api/v1/products');
+      return promise;
+    }
+
+    const products = await getProducts();
+    console.log(products.data);
+
+    async function getProductsAsync(){
+      const products = await axios.get('https://api.escuelajs.co/api/v1/products');
+      return products.data;
+    }
+
+    const rta2 = getProductsAsync();
+    console.log(rta2);
   }
 )();
 
